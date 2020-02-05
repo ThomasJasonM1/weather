b808a53ee854135ecd31e6bd025bc29046ab6839
ThomasJasonM1 <jason@call-em-all.com>
2020-02-04 23:54:35 -0600

    Adds some default HTML to the page

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,15 @@
 const baseUrl = 'http://api.openweathermap.org/data/2.5/';
 const appID = '&APPID=8ed0861da9937636b9473061da83c43c';
 const defaultCities = [];
-const usCities = masterCityList; // .filter(f => f.country === 'US' && f.stat.population > 200000);
-
-console.log(usCities.length);
+const usCities = masterCityList.filter(f => f.country === 'US' && f.stat.population > 600000);
 
 while (defaultCities.length <= 9) {
     let randomNumber = Math.floor(Math.random() * usCities.length)
     let cityId = usCities[randomNumber].id;
 
-    console.log(cityId);
     if (!defaultCities.includes(cityId)) {
         defaultCities.push(cityId);
     }
-
-    console.log(defaultCities);
 }
 
 const getWeather = queryParams => {
@@ -27,8 +22,7 @@ const getWeather = queryParams => {
 }
 
 $(document).ready(() => {
-    // console.log(list.filter(f => f.country === 'US'));
     // getWeather(
-    //     'group?id=' + JSON.stringify(defaultCities) + '&units=metric'
+    //     'group?id=' + JSON.stringify(defaultCities).replace(/[\[\]']+/g,'') + '&units=metric'
     // );
 })
