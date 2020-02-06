d9973d83d4082900b08b3cb82848f63493cc0ee3
ThomasJasonM1 <jason@call-em-all.com>
2020-02-06 13:05:07 -0600

    More updates

--- assets/script.js
+++ assets/script.js
@@ -18,11 +18,59 @@ const getWeather = queryParams => {
         method: 'GET',
     }).then((resp) => {
         console.log(resp);
+        resp.list.map(city => {
+            let li = $('<li>').attr('class', 'list-group-item list-group-item-action').text(`${city.name}, ${city.sys.country}`);
+            li.attr('data-id', city.id);
+            li.attr('data-temp', city.main.temp);
+            li.attr('data-humidity', city.main.humidity);
+            li.attr('data-wind-speed', city.wind.speed);
+            li.attr('data-icon', city.weather[0].icon);
+            li.attr('data-date', city.dt);
+            li.attr('data-name', city.name);
+            li.attr('data-lat', city.coord.lat);
+            li.attr('data-lon', city.coord.lon);
+            $('#city-list').append(li);
+        });
     })
 }
 
+const buildCityDisplay = (cityObj, cityData) => {
+    // http://openweathermap.org/img/wn/10d@2x.png
+    console.log(cityObj);
+    console.log(cityData);
+    $.ajax({
+        method: 'GET',
+        url: `${baseUrl}uvi?lat=${cityData.lat}&lon=${cityData.lon + appID}`,
+    }).then((uvIndev) => {
+        // now I have all the data to buld my card
+    })
+}
+
+$(document).on('click', '.list-group-item', function() {
+    let  numOfDays = '5';
+    let cityId = $(this).attr('data-id');
+    let cityData = {
+        cityId: cityId,
+        temperature: $(this).attr('data-temp'),
+        humidity: $(this).attr('data-humidity'),
+        windSpeed: $(this).attr('data-wind-speed'),
+        icon: $(this).attr('data-icon'),
+        date: $(this).attr('data-date'),
+        name: $(this).attr('data-name'),
+        lat: $(this).attr('data-lat'),
+        lon: $(this).attr('data-lon'),
+    }
+
+    $.ajax({
+        url: `${baseUrl}forecast?id=${cityId}&cnt=${numOfDays + appID}`,
+        method: 'GET',
+    }).then((cityObj) => {
+        buildCityDisplay(cityObj, cityData);
+    })
+})
+
 $(document).ready(() => {
-    // getWeather(
-    //     'group?id=' + JSON.stringify(defaultCities).replace(/[\[\]']+/g,'') + '&units=metric'
-    // );
+    getWeather(
+        'group?id=' + JSON.stringify(defaultCities).replace(/[\[\]']+/g,'') + '&units=imperial'
+    );
 })
