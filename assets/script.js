16e4dece9df3f4a2f03292d4b40578746c4f061f
ThomasJasonM1 <jason@call-em-all.com>
2020-02-07 11:17:09 -0600

    Fixes an issue I wasn't getting locally

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-const baseUrl = 'http://api.openweathermap.org/data/2.5/';
+const baseUrl = 'https://api.openweathermap.org/data/2.5/';
 const appID = '&APPID=8ed0861da9937636b9473061da83c43c';
 const defaultCities = [];
 const usCities = masterCityList.filter(f => f.country === 'US' && f.stat.population > 600000);
